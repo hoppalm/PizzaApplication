be3368f7c7db3e0fb4786b18cdc793fa6882a5cb
Michael_Hoppal <hoppalm@gmail.com>
2014-11-12 23:27:56 -0700

    setting up starting other activities as an example

--- app/src/main/java/com/example/pizzaapplication/app/MainActivity.java
+++ app/src/main/java/com/example/pizzaapplication/app/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.pizzaapplication.app;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -32,5 +34,21 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void login(View view) {
+        //TO DO AUTHENTICATE
+        Intent intent = new Intent(this, CustomerScreen.class);
+        startActivity(intent);
+    }
+
+    public void nonUser(View view) {
+        //Intent intent = new Intent(this, CustomerScreen.class);
+        //startActivity(intent);
+    }
+
+    public void createAccount(View view) {
+        Intent intent = new Intent(this, CreateAccount.class);
+        startActivity(intent);
     }
 }
