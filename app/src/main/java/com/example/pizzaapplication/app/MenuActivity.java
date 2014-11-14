109494ac5f61c1c655fb357274b63715048d0541
Michael_Hoppal <hoppalm@gmail.com>
2014-11-13 20:05:53 -0700

    menu done

--- app/src/main/java/com/example/pizzaapplication/app/MenuActivity.java
+++ app/src/main/java/com/example/pizzaapplication/app/MenuActivity.java
@@ -4,14 +4,71 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
 
 public class MenuActivity extends ActionBarActivity {
+
+    List<PizzaMenuItem> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ListView listView = (ListView) findViewById(R.id.menuList);
+
+        items = new ArrayList<>();
+        items.add(new PizzaMenuItem("","pizza",10));
+        items.add(new PizzaMenuItem("meat everyone","meat pizza",8));
+        items.add(new PizzaMenuItem("","soda",3));
+        items.add(new PizzaMenuItem("Whats the difference?","coke",2));
+        items.add(new PizzaMenuItem("cheesy","cheese pizza",11));
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+
+        List<String> quantities = new ArrayList<String>();
+        quantities.add("1");
+        quantities.add("2");
+        quantities.add("3");
+        quantities.add("4");
+        quantities.add("5");
+        ArrayAdapter<String> dataAdapter= new ArrayAdapter<String>(this, R.layout.simple_spinner_item , quantities);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(dataAdapter);
+
+        ArrayAdapter<PizzaMenuItem> adapter= new ArrayAdapter<PizzaMenuItem>(this, R.layout.simple_list_item_1 , items);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position,long arg3) {
+                view.setSelected(true);
+            }
+        });
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
 
